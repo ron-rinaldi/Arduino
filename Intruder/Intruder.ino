9c5be4642a7473df24ca5973ff27e5ce93c90eef
Ron on hpblue <ron@rrinaldi.com>
2017-10-15 09:55:14 -1000

    Added i2c_sender and Volume sound lib

--- Intruder/Intruder.ino
+++ Intruder/Intruder.ino
@@ -6,6 +6,8 @@
      GND  ECHO  TRIG  5V
      
 */
+#include <Wire.h>
+
 const int trigPin = 12;
 const int echoPin = 13;
 const int led1Pin = 5;
@@ -14,11 +16,15 @@ const int led3Pin = 7;
 
 const int use_serial = 0;
 
-const int leddistance1 = 15;
-const int leddistance2 = 30;
-const int leddistance3 = 60;
+const int leddistance1 = 30;
+const int leddistance2 = 60;
+const int leddistance3 = 120;
 
 void setup() {
+  // Setup I2C communication via slave #8
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+
   if (use_serial) Serial.begin( 115200);
   // Set up the ultrasonic sensor pins
   pinMode( trigPin, OUTPUT);
@@ -42,6 +48,7 @@ int measure() {
 
   duration = pulseIn( echoPin, HIGH);
   distance = (duration / 2)/ 29.1;
+  //distance = distance / 2.54;
   return distance;
 }
 
@@ -56,10 +63,15 @@ void printdistance( int distance) {
   Serial.println(" cm");
 }
 
-
+int distance;
 void loop() {
 
-  int distance = measure();
+  int d1 = measure();
+  int d2 = measure();
+  int d3 = measure();
+  int d4 = measure();
+  int d5 = measure();
+  distance = (d1+d2+d3+d4+d5) / 5;
 
   if (use_serial) printdistance( distance);
 
@@ -67,5 +79,14 @@ void loop() {
   set_led( led2Pin, distance, leddistance2);
   set_led( led3Pin, distance, leddistance3);
 
-  delay(100); 
+  delay(40); 
+}
+
+// function that executes whenever data is requested by 
+// master this function is registered as an event, 
+// see setup()
+void requestEvent() {
+  //Wire.write("1234\n "); // respond with message of 6 bytes
+  Wire.write( distance);
+  // as expected by master
 }
